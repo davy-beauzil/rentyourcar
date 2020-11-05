ac99b5d766b7fa16c788506c6d9c1fe7afaa538e
Davy Beauzil <davy@MacBook-Pro-de-Davy.local>
2020-11-05 08:56:18 +0100

    JEUDI 05 NOV 08:56

---
+++ resources/views/header.blade.php
@@ -0,0 +1,24 @@
+<header>
+	<!-- NavBar -->	
+	<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+		<a class="navbar-brand" href="#">RentYourCarhubert</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+		<ul class="nav navbar-nav ml-auto">
+				<li><a class="nav-link" href="accueil.php">Accueil</a></li>
+				<li><a class="nav-link" href="vehicule.php">Vehicule</a></li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="crud.php" id="navbardrop" data-toggle="dropdown">Location</a>
+					<div class="dropdown-menu">
+						<a class="dropdown-item" href="reservation.php">Reservation</a>
+						<a class="dropdown-item" href="historique.php">Historique</a>
+					</div>
+				</li>
+				<li><a class="nav-link" href="contact.php">Contact</a></li>
+				<li><button class="btn btn-danger navbar-btn">Connexion</button></li>
+		</ul>
+	</nav>
+
+</header>
